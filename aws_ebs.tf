1c2185925b336dc3627fdd5350c56d3033370414
|M| <manicprone@gmail.com>
2021-10-25 09:19:20 -0600

    Added rpc-address flag for electrs

--- aws_ebs.tf
+++ aws_ebs.tf
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 
 # bitcoin
-resource "aws_volume_attachment" "chain" {
+resource "aws_volume_attachment" "bitcoin" {
   device_name = var.bitcoin_ebs_volume_device_name
   volume_id   = var.bitcoin_ebs_volume_id
   instance_id = aws_instance.main.id
